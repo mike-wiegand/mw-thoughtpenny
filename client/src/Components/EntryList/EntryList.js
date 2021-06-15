372509cba3751fdd22b6f037ea560c36526684c7
mike wiegand <mike.s.wiegand@gmail.com>
2021-06-15 15:00:41 -0400

    Initial styling complete

--- client/src/Components/EntryList/EntryList.js
+++ client/src/Components/EntryList/EntryList.js
@@ -5,28 +5,40 @@ import "./EntryList.scss"
 function EntryList() {
     return (
         <section className="entryList">
-            <Link to={"/entries/"} className="entryList">
-                <div className="entryList__card">
-                    <div className="entryList__entryContainer">
-                        <h2 className="entryList__titleEntry">Quality of Mind</h2>
-                        <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
-                    </div>
+            <Link to={"/entries/"} className="entryList__linkCard">
+                <div className="entryList__entryContainer">
+                    <h2 className="entryList__titleEntry">Quality of Mind</h2>
+                    <p className="entryList__contentsEntry">The things you think about determine the quality of your mind. The things you think about determine the quality of your mind. The things you think about determine the quality of your mind. The things you think about determine the quality of your mind.</p>
                 </div>
             </Link>
-            <Link to={"/entries/"} className="entryList">
-                <div className="entryList__card">
-                    <div className="entryList__entryContainer">
-                        <h2 className="entryList__titleEntry">Quality of Mind</h2>
-                        <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
-                    </div>
+            <Link to={"/entries/"} className="entryList__linkCard">
+                <div className="entryList__entryContainer">
+                    <h2 className="entryList__titleEntry">Quality of Mind</h2>
+                    <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
                 </div>
             </Link>
-            <Link to={"/entries/"} className="entryList">
-                <div className="entryList__card">
-                    <div className="entryList__entryContainer">
-                        <h2 className="entryList__titleEntry">Quality of Mind</h2>
-                        <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
-                    </div>
+            <Link to={"/entries/"} className="entryList__linkCard">
+                <div className="entryList__entryContainer">
+                    <h2 className="entryList__titleEntry">Quality of Mind</h2>
+                    <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
+                </div>
+            </Link>
+            <Link to={"/entries/"} className="entryList__linkCard">
+                <div className="entryList__entryContainer">
+                    <h2 className="entryList__titleEntry">Quality of Mind</h2>
+                    <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
+                </div>
+            </Link>
+            <Link to={"/entries/"} className="entryList__linkCard">
+                <div className="entryList__entryContainer">
+                    <h2 className="entryList__titleEntry">Quality of Mind</h2>
+                    <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
+                </div>
+            </Link>
+            <Link to={"/entries/"} className="entryList__linkCard">
+                <div className="entryList__entryContainer">
+                    <h2 className="entryList__titleEntry">Quality of Mind</h2>
+                    <p className="entryList__contentsEntry">The things you think about determine the quality of your mind.</p>
                 </div>
             </Link>
         </section>
--- client/src/Components/TagsList/TagsList.js
+++ client/src/Components/TagsList/TagsList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./TagsList.scss"
+import { Link } from "react-router-dom";
 
 function TagsList() {
     return (
         <section className="tagsList">
-            <h2 className="tagsList__tag">#mindfulness</h2>
-            <h2 className="tagsList__tag">#stoicism</h2>
-            <h2 className="tagsList__tag">#actualization</h2>
+            <Link className="tagsList__link"><h2 className="tagsList__tag">#mindfulness</h2></Link>
+            <Link className="tagsList__link"><h2 className="tagsList__tag">#stoicism</h2></Link>
+            <Link className="tagsList__link"><h2 className="tagsList__tag">#actualization</h2></Link>
         </section>
     );
 }
