2e21920c809ff3ec9ee319e93051f89484f8e540
mike wiegand <mike.s.wiegand@gmail.com>
2021-06-20 13:25:34 -0400

    login page

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.scss"
+import logoLogin from "../../assets/Images/logo_colored_transparent.png";
 
 function Login() {
-    <div></div>
+    return (
+    <section className="login">
+        <div className="login__background">
+            <form className="login__form">
+                <div className="login__imageContainer">
+                    <img className="login__logoImg" src={logoLogin} alt="thoughtpenny"/>
+                </div>
+                <label className="login__userLabel" htmlFor="userName">Username</label>
+                <input
+                    type="text"
+                    name="userName"
+                    className="login__userEntry"
+                    placeholder="marcus.aurelius@gmail.com">
+                </input>
+                <label className="login__userLabel" htmlFor="userPwd">Password</label>
+                <input
+                    type="text"
+                    name="userPwd"
+                    className="login__userEntry"
+                    placeholder="**********">
+                </input>
+                <div className="login__buttonContainer">
+                    <Link to="/" type="submit" className="login__button" >SAVE</Link>
+                </div>
+            </form>
+        </div>
+    </section>
+    )
 }
 
 export default Login;
